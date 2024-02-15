3f3856ccd83536d2e3cdb8ec36df5af8a598cdf1
Bishal Magar <147643400+bishalmagar-git@users.noreply.github.com>
2024-02-15 22:32:21 +0545

    Update myApp.js

--- src/myApp.js
+++ src/myApp.js
@@ -1,16 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SingleBlog from "./pages/SingleBlog";
 
 function App() {
-    const [name,setName] = useState("Bishal Magar")  //setName helps to change the name.
-  return (
-   <>
-     <button onClick={()=> setName("Bishwa Raj")}>
-     {name}
-     </button> 
+  const [data, setData] = useState([]);
 
-   </>
+  const [loading, setLoading] = useState(true);
+  const fetchBlog = async () => {
+    try {
+      const res = await axios.get(
+        "https://65cd9528c715428e8b3e9af4.mockapi.io/blog"
+      );
+      if (res.status === 200) {
+        setLoading(false);
+        setData(res.data);
+      }
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(3, 1fr)",
+                gap: "30px",
+                padding: "30px",
+              }}
+            >
+              {loading
+                ? "loading..."
+                : data.map((row) => {
+                    return <Card key={row.id} row={row} />;
+                  })}
+              {}
+            </div>
+          }
+        />
+
+        <Route path="/:id" element={<SingleBlog />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
